2de8de569f1363808cff9c1a0ebfe0aa0334f628
maxut0 <lupatosreh@gmail.com>
2022-03-11 21:55:40 +0300

    49hv45s

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,5 +1,6 @@
 const {Type} = require('../models/models')
 const ApiError = require('../error/ApiError')
+
 class TypeController {
     async create(req, res) {
         const {name} = req.body
@@ -8,9 +9,9 @@ class TypeController {
     }
 
     async getAll(req, res) {
-
+        const types = await Type.findAll()
+        return res.json(types)
     }
-
 }
 
 module.exports = new TypeController()
